a027e5e3aa9cb4d91bcf38817bba8c238b050897
Richard Akindele <richardakindele8@gmail.comls q>
2022-10-17 18:23:36 -0500

    shell setup

---
+++ src/test/java/tech/richard/springbootgraphql/SpringbootGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.richard.springbootgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
